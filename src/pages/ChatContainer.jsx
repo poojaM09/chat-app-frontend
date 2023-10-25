ea9d8e6f05a9ea4028f64998bdfe0ae7c614cbe4
pooja mishra <pooja.mishra@plutustec.com>
2023-10-25 13:06:37 +0530

    I have Fixed Mobile screen Chat  view

--- src/pages/ChatContainer.jsx
+++ src/pages/ChatContainer.jsx
@@ -313,16 +313,16 @@ function ChatContainer({ currentChat, currentUser, onlineUser, setChatMsgData, h
       <div className="chat-container">
         <div className="back-chat-icon">
       
-          {isMobile == false ? (
+          {/* {isMobile == false ? (
             <div className="back-icon" onClick={() => handlehide()}>
               <FontAwesomeIcon icon={faArrowLeft} />
             </div>
-          ) : null}
-          {/* <div className="back-icon p-0 mr-2 d-block d-lg-none" onClick={() => handlehide()}>
+          ) : null} */}
+          <div className="back-icon p-0 mr-2 d-block d-lg-none" onClick={() => handlehide()}>
             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 448 512">
               <path fill="#ff6c37" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
             </svg>
-          </div> */}
+          </div>
           <div className="user-container">
             <div className="user-status">
               {isCurrentUserOnline ?
